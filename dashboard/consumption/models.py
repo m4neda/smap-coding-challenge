76ca657a4f2c5f7724945a4218ccb0e019d8eda8
“m4neda” <m4neda@gmail.com>
2018-03-21 10:27:01 +0900

    Add models

--- dashboard/consumption/models.py
+++ dashboard/consumption/models.py
@@ -4,3 +4,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    user_id = models.PositiveIntegerField()
+    area = models.CharField(max_length=128)
+    tariff = models.CharField(max_length=128)
+
+    def __str__(self):
+        return '{0}'.format(self.user_id)
+
+
+class Consumption(models.Model):
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    datetime = models.DateTimeField()
+    consumption = models.PositiveIntegerField()
+
+    def __str__(self):
+        return '{0}'.format(self.user_id)
+
