cf0426b56ca38501c4d9f0172881801c9b643e18
“m4neda” <m4neda@gmail.com>
2018-03-21 10:22:38 +0900

    Add test

--- dashboard/consumption/tests.py
+++ dashboard/consumption/tests.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+from django.urls import reverse
 # Create your tests here.
+
+
+class UrlResolveTests(TestCase):
+    def test_url_resolves_to_index_view(self):
+        response = self.client.get(reverse('consumption:index'))
+        self.assertEqual(200, response.status_code)
+
+    def test_url_resolves_to_summary_view(self):
+        response = self.client.get(reverse('consumption:summary'))
+        self.assertEqual(200, response.status_code)
+
