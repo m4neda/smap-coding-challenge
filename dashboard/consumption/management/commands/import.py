aa06127f12eab59a7d89c83c91c1149f2bfe4daa
“m4neda” <m4neda@gmail.com>
2018-03-21 13:01:04 +0900

    Add custom command import method

--- dashboard/consumption/management/commands/import.py
+++ dashboard/consumption/management/commands/import.py
@@ -1,8 +1,20 @@
 from django.core.management.base import BaseCommand
+import csv
+from ...models import User
 
 
 class Command(BaseCommand):
     help = 'import data'
 
     def handle(self, *args, **options):
-        print("Implement me!")
+        path = '../data/user_data.csv'
+        with open(path) as f:
+            reader = csv.reader(f)
+            header = next(reader)
+
+            for row in reader:
+                _, created = User.objects.get_or_create(
+                    user_id=row[0],
+                    area=row[1],
+                    tariff=row[2],
+                )
