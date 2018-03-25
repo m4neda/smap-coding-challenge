600203e68df690ff33ab597311e5e4a09e627e2d
“m4neda” <m4neda@gmail.com>
2018-03-25 16:09:37 +0900

    add urls

--- dashboard/consumption/urls.py
+++ dashboard/consumption/urls.py
@@ -7,5 +7,5 @@ app_name = 'consumption'
 urlpatterns = [
     url(r'^$', SummaryView.as_view(), name='index'),
     url(r'^summary/', SummaryView.as_view(), name='summary'),
-    url(r'^detail/', DetailView.as_view()),
+    url(r'^detail/', DetailView.as_view(), name='detail'),
 ]
