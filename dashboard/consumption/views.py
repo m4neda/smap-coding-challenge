6dc4dac6dab705f8428ca4f81f9f2af08a5b3772
“m4neda” <m4neda@gmail.com>
2018-03-25 12:00:07 +0900

    add chart object consumption_avg

--- dashboard/consumption/views.py
+++ dashboard/consumption/views.py
@@ -4,6 +4,13 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views import generic
 
+from .models import User, Consumption
+from .tables import ConsumptionTable
+
+from graphos.sources.simple import SimpleDataSource
+from graphos.renderers.morris import LineChart
+from django_pandas.io import read_frame
+from django_tables2 import RequestConfig
 # Create your views here.
 
 
@@ -12,8 +19,29 @@ class SummaryView(generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["message"] = "Hello World!!"
+        context["title"] = "Summary"
+
+        # for line Chart
+        queryset = Consumption.queryset_consumption_avg_per_day()
+        list_values = self.create_list_for_chart(queryset)
+        data_source = SimpleDataSource(data=list_values)
+        chart = LineChart(data_source)
+        context['chart'] = chart
+
         return context
+
+    @staticmethod
+    def create_list_for_chart(queryset):
+        df = read_frame(queryset)
+        # round
+        df['consumption__avg'] = df['consumption__avg'].round()
+        # First element is x axis
+        df = df.ix[:, ['day', 'consumption__avg', ]]
+        # create value list with headers
+        list_headers = df.columns.tolist()
+        list_values = df.values.tolist()
+        list_values.insert(0, list_headers)
+        return list_values
 
 
 class DetailView(generic.TemplateView):
