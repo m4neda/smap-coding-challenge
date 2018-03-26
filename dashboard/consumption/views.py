236fea816c9b1c8cf1d21f92296dc1fddc905f9c
“m4neda” <m4neda@gmail.com>
2018-03-27 08:45:33 +0900

    fix to use variable

--- dashboard/consumption/views.py
+++ dashboard/consumption/views.py
@@ -5,12 +5,10 @@ from django.shortcuts import render
 from django.views import generic
 
 from .models import User, Consumption
-from .tables import UserTable
 
 from graphos.sources.simple import SimpleDataSource
 from graphos.renderers.morris import LineChart
 from django_pandas.io import read_frame
-from django_tables2 import RequestConfig
 # Create your views here.
 
 
@@ -22,26 +20,26 @@ class SummaryView(generic.TemplateView):
         context["title"] = "Summary"
 
         # for line Chart
-        queryset = Consumption.queryset_consumption_avg_per_day()
+        queryset = Consumption.queryset_consumption_sum_per_day()
         list_values = self.create_list_for_chart(queryset)
         data_source = SimpleDataSource(data=list_values)
         chart = LineChart(data_source)
         context['chart'] = chart
+        context['chart_title'] = "Sum Consumption Line Chart"
 
         # for table
         queryset = User.objects.all().order_by('id')
         table = queryset
         context['table'] = table
+        context['table_title'] = "User List"
 
         return context
 
     @staticmethod
     def create_list_for_chart(queryset):
         df = read_frame(queryset)
-        # round
-        df['consumption__avg'] = df['consumption__avg'].round()
         # First element is x axis
-        df = df.ix[:, ['day', 'consumption__avg', ]]
+        df = df.ix[:, ['day', 'consumption__sum', ]]
         # create value list with headers
         list_headers = df.columns.tolist()
         list_values = df.values.tolist()
@@ -55,8 +53,9 @@ class DetailView(generic.TemplateView):
     def get_context_data(self, **kwargs):
 
         context = super().get_context_data(**kwargs)
-        context["message"] = "Detail View"
+        context["title"] = "Detail"
         table = Consumption.queryset_consumption_avg_per_user()
         context['table'] = table
+        context['table_title'] = "Average Consumption Per User"
         return context
 
