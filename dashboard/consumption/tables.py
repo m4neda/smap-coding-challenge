0ca2ba9628df2c44510690f2318d796d47b9410f
“m4neda” <m4neda@gmail.com>
2018-03-28 08:46:44 +0900

    remove unused

--- dashboard/consumption/tables.py
+++
@@ -1,9 +0,0 @@
-import django_tables2 as tables
-from .models import User
-
-
-class UserTable(tables.Table):
-
-    class Meta:
-        model = User
-        template_name = 'django_tables2/bootstrap.html'
