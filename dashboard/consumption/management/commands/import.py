a7d3c35bf0c14a7025a0c81bb9073c9a164b1b5c
“m4neda” <m4neda@gmail.com>
2018-03-28 19:10:33 +0900

    fix import to use variable path

--- dashboard/consumption/management/commands/import.py
+++ dashboard/consumption/management/commands/import.py
@@ -14,22 +14,26 @@ from pytz import timezone
 class Command(BaseCommand):
     help = 'import data'
 
+    def __init__(self):
+        self.user_data_path = os.path.join(settings.DATA_DIR, settings.IMPORT_USER_DATA_PATH)
+        self.consumption_path = os.path.join(settings.DATA_DIR, settings.IMPORT_CONSUMPTION_PATH)
+
     def handle(self, *args, **options):
-        user_data_path = '../data/user_data.csv'
-        consumption_path = '../data/consumption/'
+        user_data_path = self.user_data_path
+        consumption_path = self.consumption_path
 
         self.read_user_data(user_data_path)
         self.read_consumption(consumption_path)
 
     @staticmethod
     def read_consumption(path):
-        # get csv file name list
+        # get all csv file name list
         files = [relpath(x, path) for x in glob(join(path, '*.csv'))]
         for file in files:
             # separate filename ext
             user_id, _ext = os.path.splitext(file)
-
-            with open(path + file) as f:
+            filepath = os.path.join(path, file)
+            with open(filepath) as f:
                 reader = csv.reader(f, delimiter=',')
                 _header = next(reader)
                 user = User.objects.get(id=int(user_id))
@@ -37,7 +41,7 @@ class Command(BaseCommand):
                 Consumption.objects.bulk_create([Consumption(
                     user=user,
                     datetime=datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').astimezone(timezone(settings.TIME_ZONE)),
-                    consumption=float(row[1]),
+                    consumption=int(float(row[1])),
                 ) for row in reader])
 
     @staticmethod
